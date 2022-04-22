1e1456555d0c2e02510ff631e958d094ca8dfa8d
xgmak94 <xgmak94@gmail.com>
2022-04-22 13:39:16 -0700

    Added statistics and continuous play

---
+++ README.md
@@ -0,0 +1,9 @@
+This is a clone project of the popular word guessing game [Wordle](https://www.nytimes.com/games/wordle/index.html).
+
+# Introduction
+You get 6 tries to guess a 5-letter predetermined word. After each guess, the letters light up in various colors as clues. 
+Green means a letter is both correct in the word and in the correct spot.
+Yellow means a letter is in the word but is in a different spot in the word.
+Gray means a letter is not in the word at all.
+Click on the *Question Mark* symbol on the top left for more details.
+Click on the *Graph* symbol on the top right for your session statistics.
--- js/main.js
+++ js/main.js
@@ -1,14 +1,45 @@
 let allWords = new Set();
 let guessedWords = [[]];
 let firstEmptySpace = 0;
+let currentWord = ""
 
-const words = ["sweet", "onion"];
-let currentWordIndex = 0;
-let currentWord = words[currentWordIndex];
+//game statistics
+let currentStreak = 0;
+let totalWins = 0;
+let totalGames = 0;
 
-//EXAMPLE WORD RIGHT NOW
-function getRandomWord() {
-    return "dairy";
+function resetGame() {
+    initSetWords();
+    guessedWords = [[]];
+    firstEmptySpace = 0;
+    currentWord = "";
+    clearGuesses();
+}
+
+function clearGuesses() {
+    for(let i = 0 ; i < NUM_GUESSES*WORD_LENGTH ; i++) {
+        const tile = document.getElementById(i);
+        tile.textContent = undefined;
+        tile.style.backgroundColor = "";
+    }
+}
+
+async function initSetWords() { //initializes a set containing all valid words from `words.txt` file
+    function getRandomWord() {
+        let items = [];
+        allWords.forEach(v => items.push(v));
+        currentWord = items[Math.floor(Math.random() * items.length)]
+        console.log(currentWord);
+    }
+    await fetch('words.txt')
+        .then(response => response.text())
+        .then(data => {
+            all = data.split(/\r?\n/);
+            all.forEach(word => {
+                allWords.add(word);
+            });
+        });
+    getRandomWord();
 }
 
 function createHeader() {
@@ -34,7 +65,6 @@ function createGuessTiles() {
     const board = document.getElementById("board");
     for(let i = 0 ; i < NUM_GUESSES*WORD_LENGTH ; i++) {
         let tile = document.createElement("div");
-        tile.classList.add("animate__animated");
         tile.classList.add("tile");
         tile.setAttribute("id", i);
         board.appendChild(tile);
@@ -42,46 +72,46 @@ function createGuessTiles() {
 }
 
 function createKeyboard() {
+    function keyboardRow(letters, rowNum, spacers, enterAndDelete) {
+        const row = document.getElementById(`keyboard-row ${rowNum}`);
+    
+        let spacer1 = document.createElement("div");
+        let spacer2 = document.createElement("div");
+        spacer1.setAttribute("class", "spacer-half");
+        spacer2.setAttribute("class", "spacer-half");
+    
+        let ent = document.createElement("button");
+        ent.setAttribute("class", "wide-button");
+        ent.setAttribute("data-key", "Enter");
+        ent.textContent = "Enter";
+    
+        let del = document.createElement("button");
+        del.setAttribute("class", "wide-button");
+        del.setAttribute("data-key", "Delete");
+        del.textContent = "Delete";
+    
+        //adding buttons
+        for(let i = 0 ; i < letters.length ; i++) {
+            const letter = letters.charAt(i);
+    
+            let button = document.createElement("button");
+            button.setAttribute("data-key", letter);
+            button.textContent = letter;
+            row.appendChild(button);
+        }
+        if(spacers) {
+            row.insertBefore(spacer1, row.firstChild);
+            row.appendChild(spacer2);
+        }
+        if(enterAndDelete) {
+            row.insertBefore(ent, row.firstChild);
+            row.appendChild(del);
+        }
+    }
+
     keyboardRow("qwertyuiop", 1, false, false);
     keyboardRow("asdfghjkl", 2, true, false);
     keyboardRow("zxcvbnm", 3, false, true);
-}
-
-function keyboardRow(letters, rowNum, spacers, enterAndDelete) {
-    const row = document.getElementById(`keyboard-row ${rowNum}`);
-
-    let spacer1 = document.createElement("div");
-    let spacer2 = document.createElement("div");
-    spacer1.setAttribute("class", "spacer-half");
-    spacer2.setAttribute("class", "spacer-half");
-
-    let ent = document.createElement("button");
-    ent.setAttribute("class", "wide-button");
-    ent.setAttribute("data-key", "Enter");
-    ent.textContent = "Enter";
-
-    let del = document.createElement("button");
-    del.setAttribute("class", "wide-button");
-    del.setAttribute("data-key", "Delete");
-    del.textContent = "Delete";
-
-    //adding buttons
-    for(let i = 0 ; i < letters.length ; i++) {
-        const letter = letters.charAt(i);
-
-        let button = document.createElement("button");
-        button.setAttribute("data-key", letter);
-        button.textContent = letter;
-        row.appendChild(button);
-    }
-    if(spacers) {
-        row.insertBefore(spacer1, row.firstChild);
-        row.appendChild(spacer2);
-    }
-    if(enterAndDelete) {
-        row.insertBefore(ent, row.firstChild);
-        row.appendChild(del);
-    }
 }
 
 function keyboardListeners() {
@@ -105,10 +135,92 @@ function getCurrentGuess() { //will return last word as an array
 }
 
 function getCurrentWord() {
-    return words[currentWordIndex];
+    return currentWord;
 }
 
 function update(key) { //if we pressed a letter
+    async function updateEnter() { //enter pressed
+        function compareLetter(letter, guess, index) { //checks if letter exists in correct word
+            let word = getCurrentWord();
+            if(word.charAt(index) == letter) { // letter and position are correct
+                return `rgb(83, 141, 78)`;
+            }
+            else if(word.includes(letter)) {
+                return `rgb(181, 159, 59)`;
+            }
+            else {
+                return `rgb(58, 58, 60)`;
+            }
+        }
+
+        const curr = getCurrentGuess();
+        const guess = curr.join("");
+
+        //Guess is not legal, ERROR MESSAGE
+        if(curr.length != WORD_LENGTH || !isValidWord(guess)) {
+            if(curr.length != WORD_LENGTH) {
+                window.alert("Not enough letters");
+            }
+            else if(!isValidWord(guess)) {
+                window.alert("Not in word list");
+            }
+            return;
+        }
+
+        //loop through letters to change background colors
+        for(let i = 0 ; i < curr.length ; i++) {
+            tile = document.getElementById((guessedWords.length-1)*WORD_LENGTH + i);
+            tile.classList.add("animate__animated", "animate__flipInX");
+            tile.style.backgroundColor = compareLetter(curr[i], guess, i); //set color depending on letter
+        }
+        
+        if(guess == getCurrentWord()) { //correct guess
+            window.alert("Congratulations, You won!");
+            currentStreak++;
+            totalWins++;
+            totalGames++;
+            resetGame();
+            updateStatsModal();
+        }
+        else { //guess is wrong
+            if(guessedWords.length == NUM_GUESSES) { //out of chances, display correct answer
+                window.alert(`Sorry, you have no more guesses! The word was ${currentWord}.`);
+                currentStreak = 0;
+                totalGames++;
+                resetGame();
+                updateStatsModal();
+            }
+            else { //guess was wrong we still have a chance
+                guessedWords.push([]);
+            }
+        }
+    }
+    
+    function updateDelete() { //delete key pressed
+        const currentWord = getCurrentGuess();
+        currentWord.pop();
+        
+        guessedWords[guessedWords.length-1] = currentWord;
+        
+        const lastLetterElement = document.getElementById(String((guessedWords.length-1)*WORD_LENGTH + currentWord.length));
+        lastLetterElement.textContent = '';
+    
+        if(firstEmptySpace > (guessedWords.length-1)*WORD_LENGTH) { //limit to only current word
+            firstEmptySpace--;
+        }
+    }
+    
+    function updateLetter(key) { //any letter key pressed
+        const currentWord = getCurrentGuess();
+        if(currentWord && currentWord.length < WORD_LENGTH) {
+            currentWord.push(key.toLowerCase()); //store the words as lowercase
+            const element = document.getElementById(firstEmptySpace);
+    
+            firstEmptySpace++;
+            element.textContent = key;
+        }
+    }
+
     if(key == "Enter") { //enter pressed
         updateEnter();
     }
@@ -120,108 +232,19 @@ function update(key) { //if we pressed a letter
     }
 }
 
-function updateEnter() { //enter pressed
-    const currentWord = getCurrentGuess();
-    const guess = currentWord.join("");
-    if(currentWord.length != WORD_LENGTH) {
-        window.alert("Not enough letters");
-        return;
-    }
-
-    //TODO: CHECK WORD IS REAL
-    if(!isValidWord(guess)) {
-        window.alert("Not in word list");
-        return;
-    }
-
-    //TODO: FUNCTION TO SHOW CORRECT LETTERS
-    for(let i = 0 ; i < currentWord.length ; i++) {
-        tile = document.getElementById((guessedWords.length-1)*WORD_LENGTH + i);
-        tile.classList.add("animate__flipInX");
-        tile.style.backgroundColor = compareLetter(currentWord[i], guess, i); //set color depending on letter
-    }
-
-    if(guess == getCurrentWord()) { //correct guess
-        window.alert("Congratulations!")
-    }
-    else { //guess is wrong
-        if(guessedWords.length == NUM_GUESSES) { //out of chances, display correct answer
-            window.alert(`Sorry, you have no more guesses! The word was ${word}.`);
-        }
-        else { //guess was wrong we still have more chances
-            guessedWords.push([]);
-        }
-    }
-}
-
-function updateDelete() { //delete key pressed
-    const currentWord = getCurrentGuess();
-    currentWord.pop();
-
-    console.log(currentWord);
-    guessedWords[guessedWords.length-1] = currentWord;
-    
-    const lastLetterElement = document.getElementById(String((guessedWords.length-1)*WORD_LENGTH + currentWord.length));
-    lastLetterElement.textContent = '';
-
-    if(firstEmptySpace > (guessedWords.length-1)*WORD_LENGTH) { //limit to only current word
-        firstEmptySpace--;
-    }
-}
-
-function updateLetter(key) { //any letter key pressed
-    const currentWord = getCurrentGuess();
-    if(currentWord && currentWord.length < WORD_LENGTH) {
-        currentWord.push(key.toLowerCase()); //store the words as lowercase
-        const element = document.getElementById(firstEmptySpace);
-
-        firstEmptySpace++;
-        element.textContent = key;
-    }
-}
-
-function compareLetter(letter, guess, index) { //check if letter exists in correct word
-    let word = getCurrentWord();
-    if(word.charAt(index) == letter) { // letter and position are correct
-        return `rgb(83, 141, 78)`;
-    }
-    else if(word.includes(letter)) {
-        return `rgb(181, 159, 59)`;
-    }
-    else {
-        return `rgb(58, 58, 60)`;
-    }
-}
-
-//TODO:LOCAL STORAGE TO KEEP TRACK OF STATS
-function updateStatsModal() {
-    const currentStreak = window.localStorage.getItem("currentStreak") || 0;
-    const totalWins = window.localStorage.getItem("totalWins") || 0;
-    const totalGames = window.localStorage.getItem("totalGames") || 0;
-    
-    document.getElementById("total-played").textContent = totalGames;
-    document.getElementById("total-wins").textContent = totalWins;
-    document.getElementById("current-streak").textContent = currentStreak;
-
-    const winPct = Math.round((totalWins / totalGames) * 100) || 0;
-    document.getElementById("win-pct").textContent = winPct;
-}
-
 function initHelpModal() {
     const modal = document.getElementById("help-modal");
-    // Get the button that opens the modal
     const btn = document.getElementById("help");
-    // Get the <span> element that closes the modal
     const span = document.getElementById("close-help");
-    // When the user clicks on the button, open the modal
+    // User clicks on the button, open the modal
     btn.addEventListener("click", function () {
         modal.style.display = "block";
     });
-    // When the user clicks on <span> (x), close the modal
+    // User clicks on x, close the modal
     span.addEventListener("click", function () {
         modal.style.display = "none";
     });
-    // When the user clicks anywhere outside of the modal, close it
+    // User clicks outside of the modal, close it
     window.addEventListener("click", function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
@@ -231,20 +254,18 @@ function initHelpModal() {
 
 function initStatsModal() {
     const modal = document.getElementById("stats-modal");
-    // Get the button that opens the modal
     const btn = document.getElementById("stats");
-    // Get the <span> element that closes the modal
     const span = document.getElementById("close-stats");
-    // When the user clicks on the button, open the modal
+    // User clicks on the button, open the modal
     btn.addEventListener("click", function () {
         updateStatsModal();
         modal.style.display = "block";
     });
-    // When the user clicks on <span> (x), close the modal
+    // User clicks on x, close the modal
     span.addEventListener("click", function () {
         modal.style.display = "none";
     });
-    // When the user clicks anywhere outside of the modal, close it
+    // User clicks outside of the modal, close it
     window.addEventListener("click", function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
@@ -252,22 +273,17 @@ function initStatsModal() {
     });
 }
 
-function initSet() {
-    fetch('words.txt')
-        .then(response => response.text())
-        .then(data => {
-            all = data.split(/\r?\n/);
-            all.forEach(word => {
-                allWords.add(word);
-            });
-        });
+function updateStatsModal() {
+    // Set text
+    document.getElementById("total-played").textContent = totalGames;
+    document.getElementById("total-wins").textContent = totalWins;
+    document.getElementById("current-streak").textContent = currentStreak;
+
+    const winPercent = Math.round((totalWins / totalGames) * 100) || 0;
+    document.getElementById("win-pct").textContent = winPercent;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    initSet();
-
-    getRandomWord();
-
+document.addEventListener("DOMContentLoaded", async () => {
     createHeader();
     initHelpModal(); //help button for header
     initStatsModal(); //stats button for header
@@ -275,4 +291,6 @@ document.addEventListener("DOMContentLoaded", () => {
     createGuessTiles(); //create 6x5 for guesses
     createKeyboard(); //create keyboard buttons
     keyboardListeners(); //listeners for keyboard
+    
+    resetGame(); //clears everything
 });
